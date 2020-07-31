e7e316cc5b87263ea0f72ae5ff6ba655ef071217
openbsod <ispravnikov.spb@gmail.com>
2020-07-31 21:13:01 +0300

    app.py fix

--- server/app.py
+++ server/app.py
@@ -36,53 +36,54 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 @app.route('/api/img/<path:filename>')
 def sendimg(filename):
-    return send_from_directory(dir_path + '/dist/img', filename)
+    return send_from_directory(dir_path+'/dist/img', filename)
 
 
 @app.route('/js/<path:filename>')
 def sendjs(filename):
-    return send_from_directory(dir_path + '/dist/js', filename)
+    return send_from_directory(dir_path+'/dist/js', filename)
 
 
 @app.route('/css/<path:filename>')
 def sendcss(filename):
-    return send_from_directory(dir_path + '/dist/css', filename)
+    return send_from_directory(dir_path+'/dist/css', filename)
 
 
 @app.route('/api/img/icon/<path:filename>')
 def sendicon(filename):
-    return send_from_directory(dir_path + '/dist/img/icon', filename)
+    return send_from_directory(dir_path+'/dist/img/icon', filename)
 
 
 @app.route('/fonts/<path:filename>')
 def sendfonts(filename):
-    return send_from_directory(dir_path + '/dist/fonts', filename)
+    return send_from_directory(dir_path+'/dist/fonts', filename)
 
 
 @app.route('/<path:filename>')
 def sendgen(filename):
-    return send_from_directory(dir_path + '/dist', filename)
+    return send_from_directory(dir_path+'/dist', filename)
 
 
 @app.route('/')
 def index():
-    return send_from_directory(dir_path + '/dist', 'index.html')
+    return send_from_directory(dir_path+'/dist', 'index.html')
 
 
-class WebApp:
+class webapp:
     def __init__(self):
         self.camera = camera
 
-    def modeselect(self, modeinput):
-        Camera.modeSelect = modeinput
+    def modeselect(self, modeInput):
+        Camera.modeSelect = modeInput
 
-    def colorfindset(self, h, s, v):
-        camera.colorfindset(h, s, v)
+    def colorFindSet(self, H, S, V):
+        camera.colorFindSet(H, S, V)
 
     def thread(self):
         app.run(host='0.0.0.0', threaded=True)
 
     def startthread(self):
-        fps_threading = threading.Thread(target=self.thread)  # Define a thread for FPV and OpenCV
-        fps_threading.setDaemon(False)  # 'True' means it is a front thread,it would close when the mainloop() closes
-        fps_threading.start()  # Thread starts
+        fps_threading=threading.Thread(target=self.thread)         #Define a thread for FPV and OpenCV
+        fps_threading.setDaemon(False)                             #'True' means it is a front thread,it would close when the mainloop() closes
+        fps_threading.start()                                     #Thread starts
+
