21c5df5f581712aea415ddf66a067bf6aeeb27bd
openbsod <ispravnikov.spb@gmail.com>
2020-08-03 01:09:30 +0300

    speech commands LEFT | RIGHT

--- server/speech.py
+++ server/speech.py
@@ -7,19 +7,18 @@
 # Date        : 2018/10/12
 import speech_recognition as sr
 import move
-import RPIservo
 import robotLight
 import time
 
-scGear = RPIservo.ServoCtrl()
-scGear.moveInit()
-
 move.setup()
 
-RL=robotLight.RobotLight()
+RL = robotLight.RobotLight()
 
-v_command=''
+v_command = ''
 speed_set = 80
+rad = 0.5
+turnWiggle = 60
+
 
 def setup():
     move.setup()
@@ -29,46 +28,45 @@ def run():
     global v_command
     # obtain audio from the microphone
     r = sr.Recognizer()
-    with sr.Microphone(device_index =0,sample_rate=48000) as source:
-        r.record(source,duration=2)
-        #r.adjust_for_ambient_noise(source)
+    with sr.Microphone(device_index=0, sample_rate=48000) as source:
+        r.record(source, duration=2)
+        # r.adjust_for_ambient_noise(source)
         print("Command?")
         audio = r.listen(source)
 
     try:
         v_command = r.recognize_sphinx(audio,
-        keyword_entries=[('forward',1.0),('backward',1.0),
-        ('left',1.0),('right',1.0),('stop',1.0)])        #You can add your own command here
+                                       keyword_entries=[('forward', 1.0), ('backward', 1.0),
+                                                        ('left', 1.0), ('right', 1.0),
+                                                        ('stop', 1.0)])  # You can add your own command here
         print(v_command)
     except sr.UnknownValueError:
         print("say again")
     except sr.RequestError as e:
         pass
 
-    #print('pre')
+    # print('pre')
 
-    if 'forward' in v_command:
-        move.motor_left(1, 0, speed_set)
-        move.motor_right(1, 0, speed_set)
-        time.sleep(2)
+    if 'forward' in v_command:  # DOES NOT WORK YET
+        move.move(speed_set, 'forward', 'no', rad)
+        time.sleep(1)
         move.motorStop()
 
     elif 'backward' in v_command:
-        move.motor_left(1, 1, speed_set)
-        move.motor_right(1, 1, speed_set)
-        time.sleep(2)
+        move.move(speed_set, 'backward', 'no', rad)
+        time.sleep(1)
         move.motorStop()
 
     elif 'left' in v_command:
-        move.motor_left(1, 0, speed_set)
-        move.motor_right(1, 0, speed_set)
-        time.sleep(2)
+        move.motor_left(1, 1, speed_set)
+        move.motor_right(1, 1, speed_set)
+        time.sleep(1)
         move.motorStop()
 
     elif "right" in v_command:
         move.motor_left(1, 0, speed_set)
         move.motor_right(1, 0, speed_set)
-        time.sleep(2)
+        time.sleep(1)
         move.motorStop()
 
     elif 'stop' in v_command:
